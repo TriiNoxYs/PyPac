b544c164be64254967533d997a966805f81d4f64
TriiNoxYs <triinoxys@gmail.com>
2015-11-30 23:56:53 +0100

    test

--- fr/triinoxys/PyPac/main.py
+++ fr/triinoxys/PyPac/main.py
@@ -1,2 +1,10 @@
 import sys, os
+from _ast import operator
+from _operator import itemgetter
+from pip._vendor import colorama
+from asyncore import loop
 
+p = itemgetter
+loop;
+print("Debug")
+#Mettre une couleur
---
+++ fr/triinoxys/PyPac/tmp.py
@@ -0,0 +1,18 @@
+'''
+Created on 30 nov. 2015
+
+@author: TriiNoxYs
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        dis 42 si tu vois ça
+        
